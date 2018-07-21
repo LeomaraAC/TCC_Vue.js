3f6f7a4686fab691467a9bf5c3bb79795967c323
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-07-21 12:49:59 -0300

    Criação do vuex

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,7 +16,27 @@ window.pt_br = require( 'vee-validate/dist/locale/pt_BR');
 window.Vuetify = require ('vuetify');
 window.Vue.use(Vuetify);
 
-// require( 'vuetify/dist/vuetify.min.css');
+window.VueGoodTablePlugin = require('vue-good-table');
+window.Vue.use(VueGoodTablePlugin);
+import 'vue-good-table/dist/vue-good-table.css';
+
+window.Vuex = require('Vuex');
+window.Vue.use(Vuex);
+
+/**
+ * Vuex
+ */
+const store = new Vuex.Store({
+    state: {
+        itens: {}
+    },
+    mutations: {
+        settItens(state, obj) {
+            state.itens = obj;
+        }
+    }
+})
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -24,11 +44,14 @@ window.Vue.use(Vuetify);
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue'));
 Vue.component('sara-breadcrumb', require('./components/shared/Breadcrumb.vue'));
 Vue.component('sara-input', require('./components/shared/Input.vue'));
 Vue.component('sara-snackbar', require('./components/shared/Snackbar.vue'));
-Vue.component('sara-tabela', require('./components/shared/Tabela.vue'));
+Vue.component('sara-tabela', require('./components/shared/tabela/Tabela.vue'));
+Vue.component('sara-checkbox', require('./components/shared/Checkbox.vue'));
+Vue.component('sara-pagination', require('./components/shared/tabela/Pagination.vue'));
+Vue.component('sara-modal', require('./components/shared/modal/Modal.vue'));
+Vue.component('sara-linkmodal', require('./components/shared/modal/LinkModal.vue'));
 
 Vue.component('sara-login', require('./components/auth/Login.vue'));
 
@@ -36,10 +59,15 @@ Vue.component('sara-sidebar', require('./components/layouts/Sidebar.vue'));
 Vue.component('sara-card', require('./components/layouts/Card.vue'));
 Vue.component('sara-navbar', require('./components/layouts/Navbar.vue'));
 Vue.component('sara-app', require('./components/layouts/App.vue'));
+Vue.component('sara-criargrupo', require('./components/layouts/CriarGrupo.vue'));
+Vue.component('sara-listagem', require('./components/layouts/Listagem.vue'));
+Vue.component('sara-tabelapaginada', require('./components/layouts/TabelaPaginada.vue'));
 
 const app = new Vue({
     el: '#app',
+    store,
     mounted: function () {
         document.getElementById('app').style.visibility = 'visible';
+        this.$validator.localize("pt_BR");
     }
 });
