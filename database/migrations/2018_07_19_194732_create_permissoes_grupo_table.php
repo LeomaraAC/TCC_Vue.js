ecc57661435e8a3be81003e38366419b6f0048e7
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-07-21 12:44:30 -0300

    Criação das migrations do projeto

---
+++ database/migrations/2018_07_19_194732_create_permissoes_grupo_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermissoesGrupoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissoes_grupo', function (Blueprint $table) {
+            $table->integer('idGrupo')->unsigned();
+            $table->integer('idTelas')->unsigned();
+            $table->primary(['idGrupo', 'idTelas']);
+
+            $table->foreign('idGrupo')->references('idGrupo')->on('grupo_usuarios')->onDelete('cascade');
+            $table->foreign('idTelas')->references('idTelas')->on('permissoes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissoes_grupo');
+    }
+}
