2d56a5ddac119b889476c9d400871517bd154b4f
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-07-22 00:07:25 -0300

    Adicionando o componente da tabela vue-good-table

--- resources/assets/js/components/layouts/Listagem.vue
+++ resources/assets/js/components/layouts/Listagem.vue
@@ -14,51 +14,60 @@
             <a :href="linknovo" class="btn btn-outline-info"><i class="fas fa-plus"></i> Novo</a>
         </div>
     </span>
-</sara-card>
+</s-card>
 <hr>
-<sara-card :footer='pagination.last_page > 1'>
+<s-card :footer='pagination.last_page > 1'>
     <span slot="body">
-        <sara-tabela  :conteudotabela="dados" :header="header" @sort="sort" :filtroinicial="sortProperty" 
-        :select="false" :exceto="listaExceto" :issort="true" :apagar="apagar" :editar="editar"></sara-tabela>
+        <vue-good-table
+            mode="remote"
+            :columns="columns"
+            :rows="rows"
+            @on-sort-change="sort"
+            :sort-options="{ enabled: true }"
+            styleClass="table table-hover">
+      </vue-good-table>
     </span>
     <span slot="footer">
-        <sara-pagination :souce="pagination" @navigate="buscaDados" :pages="pages"></sara-pagination>
+        <s-pagination :souce="pagination" @navigate="buscaDados" :pages="pages"></s-pagination>
     </span>
-</sara-card>
+</s-card>
 </div>
 </template>
 
 <script>
     export default {
-        props:[ 'linknovo', 'linkfiltro', 'titulo', "filtroinicial", 'header', 'exceto','apagar', 'editar'],
+        props:[ 'linknovo', 'linkfiltro', 'titulo', "filtroinicial", 'columns','apagar', 'editar'],
         data: function () {
             return {
-                dados: {},
                 pages: [],
                 pagination: {},
                 busca: '',
                 sortProperty: this.filtroinicial,
                 sortDirection: 'asc',
-                listaExceto: this.exceto || []
+                listaExceto: this.exceto || [],
+                rows: [ ],
             }
         },
         mounted() {
             this.buscaDados();
         },
         methods: {
+            selectionChanged(v) {
+                console.log(v);
+            },
             buscaDados(page = 1) {
                 var url = this.busca === '' ? this.linkfiltro+'/'+this.sortProperty+'/'+this.sortDirection+'?page='+page
                                           : this.linkfiltro+'/'+this.sortProperty+'/'+this.sortDirection+'/'+this.busca+'?page='+page;
                 
                 axios.get(url).then (res => {
-                    this.dados = res.data.data;
+                    this.rows = res.data.data;
                     this.pagination = res.data;   
                     this.pages = _.range(1,this.pagination.last_page + 1)
                 });
             },
-            sort(order) {
-                this.sortProperty = order[0];
-                this.sortDirection = order[1];
+            sort(params) {
+                this.sortDirection = params.sortType;
+                this.sortProperty = this.columns[params.columnIndex].field;
                 this.buscaDados();
             },
             filtrar () {
@@ -67,3 +76,4 @@
         }
     }
 </script>
+
