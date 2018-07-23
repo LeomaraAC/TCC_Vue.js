7f1386c16dcc5c833564590b9ba0db7030bac7af
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-07-23 18:03:48 -0300

    Componente para criar um novo grupo

--- resources/views/master/grupos_create.blade.php
+++ resources/views/master/grupos_create.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 @section('content')
 <s-breadcrumb :caminhos="{{$breadcrumb}}"></s-breadcrumb>
-<s-criargrupo link="http://projetosara.meu/master/permissoes"></s-criargrupo>
-
+<form action="{{ route('grupos.store') }}" method="POST">
+    {{ csrf_field() }}
+    <s-criargrupo link="http://projetosara.meu/master/permissoes" old="{{old('grupo')}}">
+        <button  type="submit" class="btn btn-outline-success"> <i class="fas fa-save"></i> Salvar </button>
+        <button type="reset" class="btn btn-outline-secondary"><i class="fas fa-eraser"></i> Limpar</button>
+    </s-criargrupo>
+</form>
+@if ($errors->has('grupo'))
+    <s-snackbar cor="red" msg="{{ $errors->first('grupo') }}"></s-snackbar>
+@elseif ($errors->has('idTelas'))
+    <s-snackbar cor="red" msg="Selecione pelo menos uma permissão para poder continuar"></s-snackbar>
+@endif
 @endsection
