f0086b188cb462bc21d75df1f9ed0b045a54ee99
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-07-23 18:02:41 -0300

    Adicionando if para a exibição das mensagens de erros ou sucessos que vem dos controladores.

--- resources/views/master/grupos.blade.php
+++ resources/views/master/grupos.blade.php
@@ -3,6 +3,12 @@
 <s-breadcrumb :caminhos="{{$breadcrumb}}"></s-breadcrumb>
 <s-listagem  linknovo="{{ route('grupos.create') }}" linkfiltro="{{route('filtro')}}" 
     titulo="Grupos" filtroinicial="nomeGrupo" 
-    :columns="[{field:'nomeGrupo', label: 'Grupo'}, {field:'idGrupo', label: 'Grupo', hidden: true}]"
-    :apagar="false" :editar="false"></s-listagem>                         
+    :columns="[{field:'idGrupo', label: '', width: '50px'}, {field:'nomeGrupo', label: 'Grupo'}]"
+    apagar=" /master/grupos/" :editar="false" token="{{ csrf_token() }}"></s-listagem>    
+@if (session('success'))
+    <s-snackbar cor="green" msg="{{ session('success') }}"></s-snackbar>
+@endif         
+@if (session('error'))
+    <s-snackbar cor="red" msg="{{ session('error') }}"></s-snackbar>
+@endif            
 @endsection
