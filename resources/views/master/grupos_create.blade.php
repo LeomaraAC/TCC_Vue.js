5a47a5225628cd4142cf29f0c4241f2d6f7ba7d2
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-07-31 23:58:18 -0300

    Chamando o componente responsável pela criação do grupo

--- resources/views/master/grupos_create.blade.php
+++ resources/views/master/grupos_create.blade.php
@@ -1,13 +1,8 @@
 @extends('layouts.app')
 @section('content')
 <s-breadcrumb :caminhos="{{$breadcrumb}}"></s-breadcrumb>
-<form action="{{ route('grupos.store') }}" method="POST">
-    {{ csrf_field() }}
-    <s-criargrupo link="http://projetosara.meu/master/permissoes" old="{{old('grupo')}}">
-        <button  type="submit" class="btn btn-outline-success"> <i class="fas fa-save"></i> Salvar </button>
-        <button type="reset" class="btn btn-outline-secondary"><i class="fas fa-eraser"></i> Limpar</button>
-    </s-criargrupo>
-</form>
+<s-criargrupo link="http://projetosara.meu/master/permissoes" old="{{old('grupo')}}"
+    method="POST" action="{{ route('grupos.store') }}" token="{{ csrf_token() }}"></s-criargrupo>
 @if ($errors->has('grupo'))
     <s-snackbar cor="red" msg="{{ $errors->first('grupo') }}"></s-snackbar>
 @elseif ($errors->has('idTelas'))
