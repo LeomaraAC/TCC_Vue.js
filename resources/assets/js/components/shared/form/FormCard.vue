f3c289ad9c61237db7db618fbb8342be7ee74f1d
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-06 13:59:58 -0300

    Criação de um componente genérico que será utilizado quando for criar um formulário dentro de um card

---
+++ resources/assets/js/components/shared/form/FormCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <s-card :titulo="titulo" :footer="true"  :icon="icon">
+        <span slot="body">
+            <div  class="text-left">
+               <slot name="form"></slot>
+            </div>
+        </span>
+        <span slot="footer">
+            <slot name="botoes">
+                <button  type="button" @click.prevent="validaForm" class="btn btn-outline-primary"> 
+                    <i class="fas fa-save"></i> Salvar 
+                </button>
+                <button type="button" @click.prevent="resetForm" class="btn btn-outline-secondary">
+                    <i class="fas fa-eraser"></i> Limpar
+                </button>
+            </slot>
+        </span>
+    </s-card>
+</template>
+<script>
+export default {
+    props: {
+        titulo: {
+            type: String,
+            required: true
+        },
+        icon: {
+            type: String
+        }
+    },
+    methods: {
+        validaForm: function () {
+            this.$emit('submitForm')
+        },
+        resetForm: function () {
+            this.$emit('resetForm')
+        }
+    }
+}
+</script>
