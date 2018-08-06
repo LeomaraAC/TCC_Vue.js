cdfca4ff6de3ed6546614611df87ddc84275ad81
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-06 14:03:27 -0300

    Modificando as rotas para os componentes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -33,23 +33,32 @@ Vue.component('multiselect', Multiselect);
  */
 
 Vue.component('s-breadcrumb', require('./components/shared/Breadcrumb.vue'));
-Vue.component('s-input', require('./components/shared/Input.vue'));
+Vue.component('s-input', require('./components/shared/inputs/Input.vue'));
+Vue.component('s-checkbox', require('./components/shared/inputs/Checkbox.vue'));
+Vue.component('s-select', require('./components/shared/inputs/Select.vue'));
 Vue.component('s-snackbar', require('./components/shared/Snackbar.vue'));
-Vue.component('s-checkbox', require('./components/shared/Checkbox.vue'));
 Vue.component('s-pagination', require('./components/shared/tabela/Pagination.vue'));
+Vue.component('s-listagem', require('./components/shared/tabela/Listagem.vue'));
+Vue.component('s-tabelaremote', require('./components/shared/tabela/TabelaRemote.vue'));
 Vue.component('s-modal', require('./components/shared/Modal.vue'));
+Vue.component('s-formulario', require('./components/shared/form/Formulario.vue'));
+Vue.component('s-formcard', require('./components/shared/form/FormCard.vue'));
+Vue.component('s-card', require('./components/shared/Card.vue'));
 
-Vue.component('s-login', require('./components/auth/Login.vue'));
-
+Vue.component('s-login', require('./components/layouts/auth/Login.vue'));
+Vue.component('s-criargrupo', require('./components/layouts/master/CriarGrupo.vue'));
+Vue.component('s-criarusuario', require('./components/layouts/master/CriarUsuarios.vue'));
 Vue.component('s-sidebar', require('./components/layouts/Sidebar.vue'));
-Vue.component('s-formulario', require('./components/layouts/Formulario.vue'));
-Vue.component('s-card', require('./components/layouts/Card.vue'));
 Vue.component('s-navbar', require('./components/layouts/Navbar.vue'));
 Vue.component('s-app', require('./components/layouts/App.vue'));
-Vue.component('s-listagem', require('./components/layouts/Listagem.vue'));
 
 
-Vue.component('s-criargrupo', require('./components/grupo/CriarGrupo.vue'));
+Vue.component('s-teste', require('./components/ExampleComponent.vue'));
+
+//
+
+
+
 
 const app = new Vue({
     el: '#app',
