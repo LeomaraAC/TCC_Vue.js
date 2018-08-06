e5a460a4b5a32407463313a30d470b631476b2e9
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-06 14:05:09 -0300

    Criação de um componente para os campos input

---
+++ resources/assets/js/components/shared/inputs/Input.vue
@@ -0,0 +1,97 @@
+<template>
+<div class="form-group mb-3">
+    <div class="input-group">
+        <slot>
+            <input ref="input"
+                :id="id"
+                :name="id"
+                :type="type"
+                :placeholder="placeholder"  
+                v-validate="validate"
+                :class="{'form-control form-control-warning': true, 'is-invalid': errors.has(id) }"
+                data-vv-validate-on="focusout|input"
+                v-model="input"
+                :autofocus="autofocus"
+                :maxlength="maxlength"
+            >
+        </slot>
+        <div class="input-group-append" v-if="icon">
+            <span class="input-group-text"><i v-bind:class="icon"></i></span>
+        </div>
+    </div>
+    <div class="erro pl-1">
+        <slot name="error">
+            {{ errors.first(id) }}
+        </slot>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            icon: {
+                type: String,
+                default: ''
+            },
+            validate: {
+                type: String,
+                default: ''
+            },
+            id: {
+                type: [String, Number],
+                default: ''
+            },
+            placeholder: {
+                type: String,
+                default: ''
+            },
+            type: {
+                type: String,
+                default: 'text'
+            },
+            valor: {
+                type: String,
+                default: ''
+            },
+            autofocus: {
+                type: Boolean,
+                default: false
+            },
+            maxlength: {
+                type: Number
+            }
+        },
+        data: function () {
+            return {
+                input: this.valor || ''
+            }
+        },
+        methods: {
+            isValid: function () {
+                this.$validator.validateAll().then(result => {
+                    if (result) 
+                        this.$emit('validado', true);
+                    else
+                     this.$emit('validado', false);
+                });
+            },
+            clear: function () {
+                this.input = ''
+            },
+            setFocus: function () {
+                this.$refs.input.focus();
+            }
+        },
+         watch: {
+            input() {
+                this.$emit('value', this.input);
+            }
+        }
+    }
+</script>
+<style scoped>
+    .input-group-text {
+        background-color: white;
+    }    
+</style>
