5d516e9a217761189f8f4708b943929dbe2586e8
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-06 14:11:05 -0300

    Componente que contem a listagem dos dados, a paginação e o filtro, bem como um botão para a criação de um novo registro

---
+++ resources/assets/js/components/shared/tabela/Listagem.vue
@@ -0,0 +1,108 @@
+<template>
+<div class="text-left">
+    <s-card :titulo="titulo" :footer="true" :icon="icon">
+        <span slot="body">
+            <div class="offset-md-2 col-md-8 col-sm-12">
+                <s-input  icon="fas fa-search">
+                    <input name="busca" type="text" placeholder="Buscar...." class="form-control" autofocus 
+                    v-model="busca" @keyup="filtrar">
+                </s-input>
+            </div>
+        </span>
+        <span slot="footer">
+            <div class="text-left">
+                <a :href="linknovo" class="btn btn-outline-info"><i class="fas fa-plus"></i> Novo</a>
+            </div>
+        </span>
+    </s-card>
+    <hr>
+    <s-card :footer='false'>
+        <span slot="body">
+            <s-tabelaremote :pagination="pagination" :columns="columns" :rows="rows" :remoto="true"
+                    :sortProperty="sortProperty" :sortDirection="sortDirection" :apagar="apagar" :editar="editar" :token="token"
+                    :linkacoes="linkacoes"
+                    @ordenar="sort" @paginar="buscaDados">
+            </s-tabelaremote>
+        </span>
+    </s-card>
+</div>
+</template>
+
+<script>
+    export default {
+        props: {
+            linknovo: {
+                required: true,
+                type: String
+            },
+            linkfiltro: {
+                required: true,
+                type: String
+            },
+            titulo: {
+                required: true,
+                type: String
+            },
+            filtroinicial: {
+                required: true,
+                type: String
+            },
+            columns: {
+                required: true,
+                type: Array
+            },
+            apagar: {
+                type: Boolean,
+                default: false
+            },
+            editar: {
+                type: Boolean,
+                default: false
+            },
+            token: {
+                type: String
+            },
+            icon: {
+                type: String
+            },
+            linkacoes: {
+                type: String
+            }
+        },
+        data: function () {
+            return {
+                pagination: {},
+                busca: '',
+                sortProperty: this.filtroinicial,
+                sortDirection: 'asc',
+                rows: [],
+            }
+        },
+        mounted() {
+            this.buscaDados();
+        },
+        methods: {
+            executaForm: function(index){
+                document.getElementById(index).submit();
+            },
+            buscaDados(page = 1) {
+                var url = this.busca === '' ? this.linkfiltro+'/'+this.sortProperty+'/'+this.sortDirection+'?page='+page
+                                          : this.linkfiltro+'/'+this.sortProperty+'/'+this.sortDirection+'/'+this.busca+'?page='+page;
+                
+                axios.get(url).then (res => {
+                    this.rows = res.data.data;
+                    this.pagination = res.data;
+                });
+            },
+            sort(params) {
+                this.sortDirection = params.sortType == 'asc' ? 'desc' : 'asc';
+                this.sortProperty = this.columns[params.columnIndex].field;
+                this.buscaDados();
+            },
+            filtrar () {
+                this.buscaDados();
+            }
+        }
+    }
+</script>
+
