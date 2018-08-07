3c0fa7ff279be4ea7c77d8a6c08d65d12afd511d
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-07 17:10:18 -0300

    View para a edição de usuários

---
+++ resources/views/master/usuario/usuarios_edit.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content')
+    <s-breadcrumb :caminhos="{{$breadcrumb}}"></s-breadcrumb>
+    {{$usuario}}
+    <s-criarusuario titulo="Editar Usuário"
+                            :grupos="{{$grupos}}"
+                            method="PUT"
+                            action="/master/usuarios/{{$usuario->idUser}}"
+                            token="{{ csrf_token() }}"
+                            :valores="{
+                                                nome: '{{$usuario->nome}}', 
+                                                prontuario: '{{$usuario->prontuario}}',
+                                                email: '{{$usuario->email}}',
+                                                grupo:  '{{$usuario->idGrupo}}'
+                                            }"
+                            ></s-criarusuario>
+    @foreach ($errors->all() as $error)
+        <s-snackbar cor="red" msg="{{ $error }}"></s-snackbar>
+        @break
+    @endforeach
+@endsection
