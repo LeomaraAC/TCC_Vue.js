79c3c79217ec45792b4c96f31d928573db4b12c8
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-07 17:11:22 -0300

    Passando objeto com os antigos valores dos campos

--- resources/views/master/usuario/usuarios_create.blade.php
+++ resources/views/master/usuario/usuarios_create.blade.php
@@ -2,10 +2,19 @@
 @section('content')
     <s-breadcrumb :caminhos="{{$breadcrumb}}"></s-breadcrumb>
     <s-criarusuario titulo="Criar Usuário"
-                            icon="fas fa-users-cog"
                             :grupos="{{$grupos}}"
                             method="POST"
                             action="{{ route('usuarios.store') }}"
                             token="{{ csrf_token() }}"
+                            :valores="{
+                                                nome: '{{old('nome')}}', 
+                                                prontuario: '{{old('prontuario')}}',
+                                                email: '{{old('email')}}',
+                                                grupo:  '{{old('grupos')}}'
+                                            }"
                             ></s-criarusuario>
+    @foreach ($errors->all() as $error)
+    <s-snackbar cor="red" msg="{{ $error }}"></s-snackbar>
+        @break
+    @endforeach
 @endsection
