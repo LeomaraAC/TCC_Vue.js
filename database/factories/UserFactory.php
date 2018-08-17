2069345e58b46f92fa8e006ca6ace24b00c713e5
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-17 12:36:21 -0300

    Deixando o Id do grupo fixo para que não seja criado vários.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,8 +20,6 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
-        'idGrupo' => function () {
-            return factory(App\Grupo::class)->create()->idGrupo;
-        }
+        'idGrupo' => 1
     ];
 });
