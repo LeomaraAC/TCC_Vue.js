25a44e15420497fd33c8c72a68ab81f2aac679b4
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-17 10:13:53 -0300

    Atualização dos dados da tabela de permissões.

--- database/seeds/PermissaoTableSeeder.php
+++ database/seeds/PermissaoTableSeeder.php
@@ -14,7 +14,10 @@ class PermissaoTableSeeder extends Seeder
         $data = array(
             array('nomeTela'=>'Incluir Grupo', 'siglaTela'=> 'inGru'),
             array('nomeTela'=>'Editar Grupo', 'siglaTela'=> 'edGru'),
-            array('nomeTela'=>'Apagar Grupo', 'siglaTela'=> 'exGru')
+            array('nomeTela'=>'Apagar Grupo', 'siglaTela'=> 'exGru'),
+            array('nomeTela'=>'Incluir Usuário', 'siglaTela'=> 'inUser'),
+            array('nomeTela'=>'Editar Usuário', 'siglaTela'=> 'edUser'),
+            array('nomeTela'=>'Apagar Usuário', 'siglaTela'=> 'exUser')
         );
         DB::table('Permissoes')->insert($data);
     }
