40c385fa48f9e80d0a6ff9af417b02ea06beb3e5
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-17 10:11:57 -0300

    Atualização da factory de usuários, adicionando os campos para prontuário e o grupo a qual estará ligado

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,13 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'nome' => $faker->name,
+        'prontuario' => 'cv'.$faker->unique()->numberBetween($min = 1000000, $max = 9000000),
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'idGrupo' => function () {
+            return factory(App\Grupo::class)->create()->idGrupo;
+        }
     ];
 });
