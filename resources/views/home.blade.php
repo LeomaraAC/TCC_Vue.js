8957110c7fd6131f9ab62ffeb5de01907413e328
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-21 12:44:29 -0300

    Colocando um componente snack-bar para exibir as mensagens de erro.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,9 @@
 
 @section('content')
 <h1>Ola</h1>
+<h5>{{Auth::user()->can('editar_Grupo') == true ? 'true' : 'false'}}</h5>
+
+@if (session('error'))
+    <s-snackbar cor="red" msg="{{ session('error') }}"></s-snackbar>
+@endif         
 @endsection
