c089f417d63c4a14c91445ab8d92639633a98d93
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-21 12:26:56 -0300

    Criando as regras de permissões.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Permissao;
+use App\User;
+use App\Grupo;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,23 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        $permissoes = Permissao::all();
+        foreach($permissoes as $permissao) {
+            Gate::define($permissao->nome, function (User $user) use($permissao) {
+                return $user->hasPermission($permissao);
+            });  
+        }
+
+        $modulos = Permissao::all()->groupBy('modulo')->keys()->all();
+        foreach($modulos as $modulo) {
+            Gate::define($modulo, function (User $user) use($modulo) {
+                return $user->hasModulo($modulo);
+            }); 
+        }
+        Gate::define('administracao', function (User $user){
+            $grupo = $user->hasModulo('grupo');
+            $user = $user->hasModulo('usuario');
+            return $grupo || $user;
+        }); 
     }
 }
