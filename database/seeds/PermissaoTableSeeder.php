176f10cb30eb2594ae8db19b8797b44b77079ab5
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-21 12:28:40 -0300

    Corrigindo os campos a serem inseridos.

--- database/seeds/PermissaoTableSeeder.php
+++ database/seeds/PermissaoTableSeeder.php
@@ -12,12 +12,14 @@ class PermissaoTableSeeder extends Seeder
     public function run()
     {
         $data = array(
-            array('nomeTela'=>'Incluir Grupo', 'siglaTela'=> 'inGru'),
-            array('nomeTela'=>'Editar Grupo', 'siglaTela'=> 'edGru'),
-            array('nomeTela'=>'Apagar Grupo', 'siglaTela'=> 'exGru'),
-            array('nomeTela'=>'Incluir Usuário', 'siglaTela'=> 'inUser'),
-            array('nomeTela'=>'Editar Usuário', 'siglaTela'=> 'edUser'),
-            array('nomeTela'=>'Apagar Usuário', 'siglaTela'=> 'exUser')
+            array('descricao'=>'Incluir grupo','modulo'=>'grupo', 'nome'=> 'incluir_Grupo'),
+            array('descricao'=>'Editar grupo','modulo'=>'grupo', 'nome'=> 'editar_Grupo'),
+            array('descricao'=>'Apagar grupo','modulo'=>'grupo', 'nome'=> 'excluir_Grupo'),
+            array('descricao'=>'Visualizar grupo','modulo'=>'grupo', 'nome'=> 'visualizar_Grupo'),
+            array('descricao'=>'Incluir usuário', 'modulo'=>'usuario','nome'=> 'incluir_User'),
+            array('descricao'=>'Editar usuário', 'modulo'=>'usuario','nome'=> 'editar_User'),
+            array('descricao'=>'Apagar usuário','modulo'=>'usuario', 'nome'=> 'excluir_User'),
+            array('descricao'=>'Visualizar usuário','modulo'=>'usuario', 'nome'=> 'visualizar_User')
         );
         DB::table('Permissoes')->insert($data);
     }
