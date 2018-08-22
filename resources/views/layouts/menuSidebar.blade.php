98ed16a514227c38c2eed31fb9740635dd731e51
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-22 18:56:13 -0300

    Adicionando restrições de acesso.

--- resources/views/layouts/menuSidebar.blade.php
+++ resources/views/layouts/menuSidebar.blade.php
@@ -1,36 +1,38 @@
 <ul class="list-unstyled components">
-        <li>
-            <a href="/">
-                <i class="fas fa-tachometer-alt"></i>
-                Dashboard
-            </a>
-        </li>
-        <!-- Administração -->
-        @can('administracao')
-            <li>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                    <i class="fas fa-university"></i>
-                    Administração
-                </a>
-                <ul class="collapse list-unstyled" id="pageSubmenu">
-                    @can('grupo')
-                        <li>
-                            <a href="/master/grupos"> <i class="fas fa-shapes"></i> Grupos</a>
-                        </li>
-                    @endcan
-                    @can('usuario')
-                        <li>
-                            <a href="/master/usuarios"> <i class="fas fa-users-cog"></i> Usuários</a>
-                        </li>
-                    @endcan
-                </ul>
-            </li>
-        @endcan
-        <!-- Alunos -->
-    <li class="nav-item">
-        <a class="nav-link" href="#">
-            <i class="fas fa-user-graduate"></i>
-            Aluno
+    <li>
+        <a href="/">
+            <i class="fas fa-tachometer-alt"></i>
+            Dashboard
         </a>
     </li>
+    <!-- Administração -->
+    @can('administracao')
+        <li>
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-university"></i>
+                Administração
+            </a>
+            <ul class="collapse list-unstyled" id="pageSubmenu">
+                @can('grupo')
+                    <li>
+                        <a href="/master/grupos"> <i class="fas fa-shapes"></i> Grupos</a>
+                    </li>
+                @endcan
+                @can('usuario')
+                    <li>
+                        <a href="/master/usuarios"> <i class="fas fa-users-cog"></i> Usuários</a>
+                    </li>
+                @endcan
+            </ul>
+        </li>
+    @endcan
+        <!-- Alunos -->
+    @can('aluno')
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('alunos.index')}}">
+                <i class="fas fa-user-graduate"></i>
+                Aluno
+            </a>
+        </li>
+    @endcan
 </ul>
