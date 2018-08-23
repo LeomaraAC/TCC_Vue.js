24f373eec5fba6376528203dd8f16c90c1432f78
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-08-23 09:47:25 -0300

    Componente vue.js para a busca a pesquisa no BD.

---
+++ resources/assets/js/components/shared/inputs/Pesquisa.vue
@@ -0,0 +1,27 @@
+<template>
+    <s-input  icon="fas fa-search" :btn="true">
+        <input name="busca" type="text" placeholder="Buscar...." class="form-control" autofocus
+            v-model="search" @keyup.enter="filtrar">
+    </s-input>
+</template>
+<script>
+export default {
+    data: function () {
+        return {
+            search: ''
+        }
+    },
+    methods:{
+        filtrar: function () {
+            Event.fire('filtrar', this.search);
+        }
+    },
+    mounted: function() {
+        Event.listen('btnInput', () => {
+            console.log('ouvi');
+            this.filtrar()
+        });
+    }
+}
+</script>
+
