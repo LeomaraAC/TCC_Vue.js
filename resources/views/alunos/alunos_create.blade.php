03c1957de62ec044cd2e2d07f00af5d67d1e6bb3
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-09-03 14:09:01 -0300

    View para o cadastro de alunos.

---
+++ resources/views/alunos/alunos_create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+    <s-breadcrumb :caminhos="{{$breadcrumb}}"></s-breadcrumb>
+    @foreach ($errors->all() as $error)
+        <h2>Finalmente</h2>
+        <s-snackbar cor="red" msg="{{ $error }}"></s-snackbar>
+        @break
+    @endforeach
+    <s-formularioalunos action="{{route('alunos.store')}}" token="{{csrf_token()}}"
+    method="POST" :valores="{
+                                nome: '{{old('nome')}}', 
+                                prontuario: '{{old('prontuario')}}',
+                                email: '{{old('email')}}',
+                                telefone:  '{{old('telefone')}}',
+                                semestre:  '{{old('semestre')}}',
+                                observacao:  '{{old('observacao')}}',
+                                curso:  '{{old('curso')}}',
+                            }"/>
+
+@endsection
