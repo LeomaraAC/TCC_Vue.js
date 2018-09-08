29f60ce49ee4e12da7a3d532dd8b6effc4db25ef
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-09-08 19:41:45 -0300

    Criação da tabela de matriculas dos alunos

---
+++ database/migrations/2018_09_06_214914_create_matricula_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatriculaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matricula', function (Blueprint $table) {
+            $table->string('prontuario');
+            $table->integer('cpf');
+            $table->string('codigo_curso');
+            $table->year('previsao_conclusao');
+            $table->year('ano_ingresso');
+            $table->date('data_integralizacao')->nullable();
+            $table->string('forma_ingresso');
+            $table->string('instituicao_anterior')->nullable();
+            $table->string('situacao_curso');
+            $table->string('situacao_periodo')->nullable();
+            $table->string('turma')->nullable();
+            $table->string('email_academico')->nullable();
+            $table->text('observacao_historico')->nullable();
+
+            $table->primary('prontuario');
+            $table->foreign('cpf')->references('cpf')->on('alunos')->onDelete('cascade');
+            $table->foreign('codigo_curso')->references('codigo')->on('cursos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matricula');
+    }
+}
