f185a8932b92bcdcb9f2932ec306722785facddb
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-09-08 19:41:24 -0300

    Criação da tabela de endereço

---
+++ database/migrations/2018_09_06_214336_create_endereco_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEnderecoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('endereco', function (Blueprint $table) {
+            $table->increments('idEndereco');
+            $table->string('rua', 100);
+            $table->string('numero', 8);
+            $table->string('bairro', 100);
+            $table->integer('cep');
+            $table->string('cidade', 100);
+            $table->string('estado', 2);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('endereco');
+    }
+}
