5461a00d2ec8829f15d6cbe317a4a2e370491efa
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-09-11 19:58:54 -0300

    Criação do repositor responsável pelas interações realizadas com a tabela de telefone.

---
+++ app/Repositories/TelefoneRepository.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\ Repositories;
+
+use App\Repositories\Contracts\Base\BaseRepository;
+use App\Telefone;
+
+class TelefoneRepository  extends  BaseRepository
+{
+    protected $model;
+
+    public function __construct(Telefone $telefone) {
+        $this->model = $telefone;
+    }
+}
