76dac09b682fafeb2444ff95d5ebd9186fb2d81c
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-10-08 14:54:49 -0300

    Remoção de permissões que não estão sendo utilizadas.

--- database/seeds/PermissaoTableSeeder.php
+++ database/seeds/PermissaoTableSeeder.php
@@ -20,9 +20,6 @@ class PermissaoTableSeeder extends Seeder
             array('descricao'=>'Editar usuário', 'modulo'=>'usuario','nome'=> 'editar_User'),
             array('descricao'=>'Apagar usuário','modulo'=>'usuario', 'nome'=> 'excluir_User'),
             array('descricao'=>'Visualizar usuário','modulo'=>'usuario', 'nome'=> 'visualizar_User'),
-            array('descricao'=>'Incluir aluno', 'modulo'=>'aluno','nome'=> 'incluir_Aluno'),
-            array('descricao'=>'Editar aluno', 'modulo'=>'aluno','nome'=> 'editar_Aluno'),
-            array('descricao'=>'Apagar aluno','modulo'=>'aluno', 'nome'=> 'excluir_Aluno'),
             array('descricao'=>'Importar dados dos alunos','modulo'=>'aluno', 'nome'=> 'importar_Alunos'),
             array('descricao'=>'Visualizar aluno','modulo'=>'aluno', 'nome'=> 'visualizar_Aluno')
         );
