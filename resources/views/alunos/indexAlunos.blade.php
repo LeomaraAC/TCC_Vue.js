12883e31b703e958fc65e2a02c624562bce1655d
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-10-08 14:50:46 -0300

    Remoção do botão para a inclusão de novos alunos.

--- resources/views/alunos/indexAlunos.blade.php
+++ resources/views/alunos/indexAlunos.blade.php
@@ -2,14 +2,13 @@
 @section('content')
     <s-breadcrumb :caminhos="{{$breadcrumb}}"></s-breadcrumb>
     <s-card titulo="Alunos" icon="fas fa-user-graduate"
-        :footer="{{Auth::user()->can('incluir_Aluno') == true ? 'true' : 'false'}}">
+        :footer="{{Auth::user()->can('importar_Alunos') == true ? 'true' : 'false'}}">
         <span slot="body">
             <div class="offset-md-2 col-md-8 col-sm-12">
                 <s-pesquisa></s-pesquisa>
             </div>
         </span>
-        <div slot="footer" v-if="{{Auth::user()->can('incluir_Aluno') == true ? 'true' : 'false'}} || 
-                                {{Auth::user()->can('importar_Alunos') == true ? 'true' : 'false'}}" class="row">
+        <div slot="footer" v-if="{{Auth::user()->can('importar_Alunos') == true ? 'true' : 'false'}}" class="row">
             
             @if(Auth::user()->can('importar_Alunos'))
                 <s-modalimport token="{{csrf_token()}}" />
