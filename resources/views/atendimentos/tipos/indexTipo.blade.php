0c793749691ed5c74a52fc70e40778c0f274239e
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-10-09 20:09:19 -0300

    Movendo o código para a exibição das mensagens de sucesso ou erro para o top do arquivo.

--- resources/views/atendimentos/tipos/indexTipo.blade.php
+++ resources/views/atendimentos/tipos/indexTipo.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 @section('content')
+    @if (session('success'))
+        <s-snackbar cor="green" msg="{{ session('success') }}"></s-snackbar>
+    @endif         
+    @if (session('error'))
+        <s-snackbar cor="red" msg="{{ session('error') }}"></s-snackbar>
+    @endif    
     <s-breadcrumb :caminhos="{{$breadcrumb}}"></s-breadcrumb>
     <s-card titulo="Tipos de atendimento" icon="fas fa-tag"
         :footer="{{Auth::user()->can('incluir_Tipo_Atendimento') == true ? 'true' : 'false'}}">
@@ -8,25 +14,18 @@
                 <s-pesquisa></s-pesquisa>
             </div>
         </span>
-        <div slot="footer" v-if="{{Auth::user()->can('incluir_Tipo_Atendimento') == true ? 'true' : 'false'}}" class="row">
+        <span slot="footer" v-if="{{Auth::user()->can('incluir_Tipo_Atendimento') == true ? 'true' : 'false'}}">
             @if(Auth::user()->can('importar_Alunos'))
                 <div class="text-left">
                     <a id="novoTipo" href="{{route('tipo.create')}}" class="btn btn-outline-info"><i class="fas fa-plus"></i> Novo</a>
                 </div>
             @endif
-        </div>
+        </span>
     </s-card>
     <hr>
     <s-listagemtipo linkacoes="{{route('tipo.index')}}" linkfiltro="{{route('tipo.filtro')}}"
         :columns="{{$columns}}" token="{{ csrf_token() }}"
         :apagar="{{Auth::user()->can('excluir_Tipo_Atendimento') == true ? 'true' : 'false'}}" 
         :editar="{{Auth::user()->can('editar_Tipo_Atendimento') == true ? 'true' : 'false'}}"
-    />
-
-@if (session('success'))
-    <s-snackbar cor="green" msg="{{ session('success') }}"></s-snackbar>
-@endif         
-@if (session('error'))
-    <s-snackbar cor="red" msg="{{ session('error') }}"></s-snackbar>
-@endif            
+    />        
 @endsection
