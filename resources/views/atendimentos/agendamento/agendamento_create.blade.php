d8007d39e24e7e1ec52fe86063342c58b84073d5
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-10-25 12:38:55 -0300

    View para a criação do agendamento.

---
+++ resources/views/atendimentos/agendamento/agendamento_create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+    
+    <s-breadcrumb :caminhos="{{$breadcrumb}}"></s-breadcrumb>
+    <s-formularioagendamento method="POST" action="{{ route('agendamento.store') }}" 
+                        token="{{ csrf_token() }}" :alunos="{{$alunos}}"/>
+@endsection
