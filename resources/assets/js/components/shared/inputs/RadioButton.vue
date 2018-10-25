1145a630e5412c9d47bd67d9ecae153104f901fa
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-10-25 12:38:22 -0300

    Componente vuejs contendo o radio button personalizado.

---
+++ resources/assets/js/components/shared/inputs/RadioButton.vue
@@ -0,0 +1,44 @@
+<template>
+    <label class="check ">{{label}}
+        <input type="radio" :name="name" :value="value" @click="checked"  :checked="ischecked ? 'checked' : ''">
+        <span class="checkmark"></span>
+    </label>
+</template>
+<script>
+    export default {
+        props: {
+            label: {
+                type: String,
+                default: ""
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            id: {
+                type: String,
+                required: true
+            },
+            ischecked: {
+                type: Boolean,
+                default: false
+            },
+            value: {
+                type: String,
+                required: true
+            }
+        },
+        methods: {
+            checked ( ) {
+                this.$emit('checked', this.value);
+            }
+        }
+    }
+</script>
+<style scoped>
+
+.checkmark {
+  border-radius: 50%;
+}
+</style>
+
