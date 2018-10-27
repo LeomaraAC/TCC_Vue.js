61e539526428061bb573ca1e6bcc160f51ea1c53
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-10-27 20:56:42 -0300

    Essa função é chamada somente quando o agendamento é visível por todos e é responsável por fazer uma busca no banco verificando se o novo agendamento não entra em conflito com um existente.

--- app/Repositories/AgendamentoRepository.php
+++ app/Repositories/AgendamentoRepository.php
@@ -11,4 +11,35 @@ class AgendamentoRepository  extends  BaseRepository
     public function __construct(Agendamento $agendamento) {
         $this->model = $agendamento;
     }
+    private function validaHorarioVisivel($horaInicial, $horaFinal, $data) {
+       
+        $atendimentos = $this->model
+                             ->where('todos', '=', true)
+                             ->where('dataPrevisto', '=', $data)
+                             ->where(function($query) use ($horaFinal, $horaInicial) {
+                                $query->where(function($q) use ($horaFinal, $horaInicial) {
+                                   $q->where('horaPrevistaInicio', '>=', $horaInicial)
+                                     ->where('horaPrevistaFim', '<=', $horaFinal); 
+                                })
+                                ->orwhere(function($q) use ($horaFinal, $horaInicial){
+                                    $q->where('horaPrevistaInicio', '>=', $horaInicial)
+                                      ->where('horaPrevistaInicio', '<', $horaFinal)
+                                      ->where('horaPrevistaFim', '>=', $horaFinal);
+                                })
+                                ->orwhere(function($q) use ($horaFinal, $horaInicial){
+                                    $q->where('horaPrevistaInicio', '<=', $horaInicial)
+                                      ->where('horaPrevistaFim', '>', $horaInicial)
+                                      ->where('horaPrevistaFim', '<=', $horaFinal);
+                                })
+                                ->orwhere(function($q) use ($horaFinal, $horaInicial){
+                                    $q->where('horaPrevistaInicio', '<=', $horaInicial)
+                                      ->where('horaPrevistaFim', '>=', $horaFinal);
+                                });
+                             })
+                             ->count();
+        if($atendimentos == 0)
+            return true;
+        else
+            return false;
+    }
 }
