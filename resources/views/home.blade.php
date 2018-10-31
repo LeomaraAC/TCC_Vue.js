5d2898fc93ec0d140774376693ec5cf9385840b0
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-10-30 22:32:33 -0300

    Colocando dois componentes, um ao lado do outro, que irá exibir as próximas 5 reuniões e cindo das reuniões atrasadas.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Ola</h1>
-<h5>{{Auth::user()->can('editar_Grupo') == true ? 'true' : 'false'}}</h5>
+@if(Auth::user()->can('agendamento'))
+<div class="row">
+    <div class="col-md-6 col-sm-12">
+        <s-box color="success">
+            <span slot="title">
+                As próximas 5 reuniões.
+            </span>
+            <s-visualizacaohome :columns="{{$columns}}" :rows="{{$rowsProximas}}" id="proximas"></s-visualizacaohome>
+        </s-box>
+    </div>
+    <div class="col-md-6 col-sm-12">
+        <s-box color="success">
+            <span slot="title">
+                As últimas 5 reuniões atrasadas.
+            </span>
+            <s-visualizacaohome :columns="{{$columns}}" :rows="{{$rowsAtrasadas}}" id="atrasadas"></s-visualizacaohome>
+        </s-box>
+    </div>
+</div>
+@endif
+
 
 @if (session('error'))
     <s-snackbar cor="red" msg="{{ session('error') }}"></s-snackbar>
