07a9b44bb36bc774f6286e8dcace051a73d2d785
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-10-30 22:18:03 -0300

    Criação da função para o retorno dos alunos que estão relacionados a um determinado atendimento.

--- app/Repositories/AlunoRepository.php
+++ app/Repositories/AlunoRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Repositories\Contracts\Base\BaseRepository;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Aluno;
 
@@ -21,7 +22,28 @@ class AlunoRepository  extends  BaseRepository
                             ->orWhere('email_pessoal', 'like', '%'.$filter.'%')
                             ->paginate(25);
     }
+
     public function selectAll() {
         return $this->model->select('cpf', 'nome')->orderBy('nome', 'asc')->get();
+    }
+
+    public function getAlunoAgendamento($idAgendamento) {
+        return $this->model
+            ->select(
+                    'matricula.prontuario',
+                    'alunos.nome', 
+                    'alunos.responsavel',
+                    'cursos.descricao as curso',
+                    DB::raw('group_concat(telefone.numero) as telefone'))
+            ->join('telefone', 'telefone.cpf','=', 'alunos.cpf')
+            ->join('matricula', 'matricula.cpf','=', 'alunos.cpf')
+            ->join('cursos', 'cursos.codigo','=', 'matricula.codigo_curso')
+            ->join('agendamento_matricula', 'agendamento_matricula.prontuario','=', 'matricula.prontuario')
+            ->join('agendamento', 'agendamento.idAgendamento','=', 'agendamento_matricula.idAgendamento')
+            ->where('agendamento.idAgendamento', '=', $idAgendamento)
+            ->where('telefone.deleted_at', '=', null)
+            ->groupBy('matricula.prontuario')
+            ->get();
+        
     }
 }
