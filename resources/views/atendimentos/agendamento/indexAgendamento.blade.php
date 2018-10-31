d12ef79b8c5cf25239a9636d0cbdd40bd95c85a5
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-10-30 22:39:05 -0300

    View para a listagem dos agendamentos.

---
+++ resources/views/atendimentos/agendamento/indexAgendamento.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+    @if (session('success'))
+        <s-snackbar cor="green" msg="{{ session('success') }}"></s-snackbar>
+    @endif         
+    @if (session('error'))
+        <s-snackbar cor="red" msg="{{ session('error') }}"></s-snackbar>
+    @endif    
+    <s-breadcrumb :caminhos="{{$breadcrumb}}"></s-breadcrumb>
+    <s-card titulo="Atendimentos" icon="fas fa-calendar-alt"
+        :footer="{{Auth::user()->can('agendar_Atendimento') == true ? 'true' : 'false'}}">
+        <span slot="body">
+                <s-pesquisaagendamento></s-pesquisaagendamento>
+        </span>
+        <span slot="footer" v-if="{{Auth::user()->can('agendar_Atendimento') == true ? 'true' : 'false'}}">
+            @if(Auth::user()->can('agendar_Atendimento'))
+                <div class="text-left">
+                    <a id="novoTipo" href="{{route('agendamento.create')}}" class="btn btn-outline-info"><i class="fas fa-plus"></i> Novo</a>
+                </div>
+            @endif
+        </span>
+    </s-card>
+    <hr>
+    <s-listagemagendamento linkfiltro="{{route('agendamento.filtro')}}"
+        :columns="{{$columns}}"
+    />        
+@endsection
