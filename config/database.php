ff8c17bd66aa7ce7c28f7dc8f5e2a31e43384fc6
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-12-05 14:06:01 -0200

    update database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'forge', 'projetosara'),
+            'username' => env('DB_USERNAME', 'forge', 'zwdyifiodtosbm'),
+            'password' => env('DB_PASSWORD', '', '1h9RXwB0'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
