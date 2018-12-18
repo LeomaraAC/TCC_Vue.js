d4e1160141617c7cb0173505c29c7461ef694283
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-12-18 16:49:08 -0200

    Modificando configurações do heroku.

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1', 'db4free.net'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge', 'projetosara'),
-            'username' => env('DB_USERNAME', 'forge', 'zwdyifiodtosbm'),
-            'password' => env('DB_PASSWORD', '', '1h9RXwB0'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
